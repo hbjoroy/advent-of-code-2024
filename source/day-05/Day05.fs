c0fee8a9da33f22a5bcba46ed44015b11eacf580
Harald Bjorøy <harald.bjoroy@spv.no>
2024-12-06 05:57:28 +0100

    Skjelett dag 5

---
+++ source/day-05/Day05.fs
@@ -0,0 +1,22 @@
+﻿module Day05
+
+open System
+open System.Text.RegularExpressions
+open Xunit
+
+
+
+[<Theory>]
+[<InlineData("day05-01.txt",143)>]
+let ``My test`` (file, (medianSum:int)) = task {
+    let! file = IO.File.ReadAllLinesAsync(file)
+    let defs,jobs = Array.splitAt(Array.findIndex ((=) "")file ) file
+    let jobs = Array.skip 1 jobs
+
+    let m = Map<int,int[]>
+
+
+    Assert.Equal(21, defs.Length)
+    Assert.Equal(6, jobs.Length)
+}
+
